bcb2eb616590069bae4e600587a429c11a29f551
Björn Westlin <bwestlin@gmail.com>
2015-09-22 20:09:49 +0200

    Fix TODO:s

--- src/main/scala/TopicConnection.scala
+++ src/main/scala/TopicConnection.scala
@@ -1,6 +1,7 @@
 import java.util.UUID
 
 import akka.actor._
+import akka.cluster.pubsub.{DistributedPubSub, DistributedPubSubMediator}
 
 object TopicConnection {
 
@@ -17,24 +18,24 @@ object TopicConnection {
 }
 
 class TopicConnection(topic: String) extends Actor with ActorLogging {
-
   import TopicConnection._
+  import DistributedPubSubMediator.{ Subscribe, SubscribeAck }
 
   val id = UUID.randomUUID()
   var client = context.system.deadLetters
   log.info("Client session {} started", id.toString)
 
-  // TODO subscribe to topic
+  val mediator = DistributedPubSub(context.system).mediator
+  mediator ! Subscribe(topic, self)
 
   def receive = {
     case Message(text) =>
       log.info("Got message from client {}: {}", id, text)
-      // TODO publish to topic
+      mediator ! DistributedPubSubMediator.Publish(topic, DistributedMessage(id, text))
 
     case msg @ DistributedMessage(uuid, text) if uuid != id =>
       log.info("Got message from other cluster node: {}", msg)
-      // TODO send to client
-
+      client ! Message(text)
 
     case OutgoingDestination(destination) =>
       client = destination
